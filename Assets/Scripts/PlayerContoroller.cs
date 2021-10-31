00a42794269d491b93e1d2d125a49562f2e49484
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2021-10-31 15:02:18 +0900

    Jump

--- Assets/Scripts/PlayerContoroller.cs
+++ Assets/Scripts/PlayerContoroller.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class PlayerContoroller : MonoBehaviour
 {
+    [SerializeField]
+    [Range(1, 10)]
+    private float jumpValocity;
+
+    private float fallMultiflier = 2.5f;
+    private float lowJumpMultiflier = 2f;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +27,18 @@ public class PlayerContoroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.up * jumpValocity;
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiflier - 1) * Time.deltaTime;
+        } 
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiflier - 1) * Time.deltaTime;
+        }
     }
 }
