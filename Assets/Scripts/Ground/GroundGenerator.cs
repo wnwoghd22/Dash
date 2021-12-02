0e02ed1a607bfa537d9bfe0e5ee9a378583d53b5
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2021-12-03 00:10:27 +0900

    modeling

--- Assets/Scripts/Ground/GroundGenerator.cs
+++ Assets/Scripts/Ground/GroundGenerator.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class GroundGenerator : MonoBehaviour
 {
+    [SerializeField]
     private GameObject groundPrefab;
     private float altitude;
+
+    private List<GroundElement> elements;
+    private float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        altitude = 2.5f;   
+        altitude = 2.5f;
     }
 
     // Update is called once per frame
